f9a676aa22a3729e01b2efdeee3c903eb0b282f6
JollyXie <381944069@qq.com>
2018-06-25 22:21:43 +0800

    finish page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>  <!--内置标签，意思是加载一次后就将访问信息保存到内存中-->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons' // 引入icons组件
 import HomeRecommend from './components/Recommend' // 引入recommend组件
 import HomeWeekend from './components/Weekend' // 引入weekend组件
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
@@ -27,15 +28,19 @@ export default {
   },
   data () {
     return {
+      lastCity: '',  // 上次访问加载时候的城市
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)  // get返回一个异步promise对象，需要回调函数
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)  // get返回一个异步promise对象，需要回调函数
     },
     getHomeInfoSucc (res) {
       res = res.data
@@ -49,7 +54,14 @@ export default {
     }
   },
   mounted () {  // 页面挂载好后
-    this.getHomeInfo()  // 获取ajax数据
+    this.getHomeInfo(),  // 获取ajax数据
+    this.lastCity = this.city  // 给城市赋值
+  },
+  activated () {  // 在再次显示的时候会调用，也就是keepalive缓存在内存中的页面重新加载时
+    if (this.lastCity !== this.city) {  // 当本次点击选中的城市不等于缓存页面中的城市时，再次请求数据
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
