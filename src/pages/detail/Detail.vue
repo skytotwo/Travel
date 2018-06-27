44eb48f6df2299256c8eed7a069a4bc94f8d529c
JollyXie <381944069@qq.com>
2018-06-27 23:31:48 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,25 @@
+<!--实现header下拉渐隐渐现的效果-->
 <template>
-  <Detail-Banner></Detail-Banner>
+  <div>
+    <Detail-Banner></Detail-Banner>
+    <Detail-Header></Detail-Header>
+    <div class="content"></div>
+  </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  
+  .content
+    height: 50rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <router-link
+      tag="div"
+      to="/"
+      class="header-abs"
+      v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>  <!--显示的返回小圆点-->
+    <div
+      class="header-fixed"
+      v-show="!showAbs"
+      :style="opacityStyle"
+    >  <!--显示主内容的header--> 
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,  // 定义header是否显示
+      opacityStyle: {
+        opacity: 0  // 隐藏显示过度样式，0代表透明
+      }
+    }
+  },
+  methods: {
+    handleScroll () {  // 调用滚动方法
+      const top = document.documentElement.scrollTop  // 滑动时，获取滚动的高度（被卷去的高度）
+      if (top > 60) {  // 根据滑动，不断进行判断，动态给过度属性opacityStyle赋值，以实现渐隐渐现的效果
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false  // 隐藏
+      } else {
+        this.showAbs = true  // 显示
+      }
+    }
+  },
+  activated () {  // 因为页面是keepalive的，这里表示页面显示
+    window.addEventListener('scroll', this.handleScroll)  // 增加事件监听，调用方法
+  },
+  deactivated () {  // 因为页面是keepalive的，这里表示页面隐藏
+    window.removeEventListener('scroll', this.handleScroll)  // 移除时间监听，调用方法
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs  // banner中圆点的样式
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    line-height: .8rem
+    border-radius: .4rem
+    text-align: center
+    background: rgba(0, 0, 0, .8)
+    .header-abs-back
+      color: #fff
+      font-size: .4rem
+  .header-fixed  // header的样式
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-fixed-back  // header中返回的样式
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
